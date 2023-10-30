47bc52200c86c32c0a79ae59b2e9682fc7e63be5
destiny3912 <destiny3910@gmail.com>
2023-10-30 12:54:15 +0900

    project init

---
+++ src/test/java/com/rookie/springbootreboot/SpringbootRebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookie.springbootreboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRebootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
